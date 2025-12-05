84ce36bb9bf018943498958c8ceb5847e58dc8d7
King <King-Nicolas@users.noreply.github.com>
2025-12-05 13:34:30 +0100

    Basic Strategy Pattern Implementation

---
+++ GildedTros.App/UpdateQuality.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedTros.App
+{
+    internal class UpdateQuality : IUpdateQuality
+    {
+        public void UpdateItemQuality(Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
