dc9ca585f195055750974a1910156a28ff88251e
King <King-Nicolas@users.noreply.github.com>
2025-12-05 16:17:47 +0100

    Cleanup

--- GildedTros.App/UpdateQualitySmellyItem.cs
+++ GildedTros.App/UpdateQualitySmellyItem.cs
@@ -8,14 +8,10 @@ namespace GildedTros.App
 {
     internal class UpdateQualitySmellyItem : UpdateQuality
     {
-        private readonly String[] _smellyItemsList = new String[] { "Duplicate Code", "Long Methods", "Ugly Variable Names" };
         public override void UpdateItemQuality(Item item)
         {
             base.baseDecayRate = 2;
-            if (_smellyItemsList.Contains(item.Name))
-            {
-                base.UpdateItemQuality(item);
-            }
+            base.UpdateItemQuality(item);
         }
     }
 }
